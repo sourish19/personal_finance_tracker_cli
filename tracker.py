6283d56a58a93f128f17286a9c63aa8c89a3207a
Sourish <sourish19z@gmail.com>
2026-09-04 00:10:19 +0530

    initialize project with boilerplate

---
+++ tracker.py
@@ -0,0 +1,14 @@
+# Keep this structure flexible
+transactions = {}  # or could be list or dict
+
+def parse_input(user_input):
+    """Parse user command and return components"""
+    pass
+
+def add_transaction(amount, category, date, description):
+    """Add expense to tracker"""
+    pass
+
+def get_total_by_category():
+    """Return dict with category totals"""
+    pass
